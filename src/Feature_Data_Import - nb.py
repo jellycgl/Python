c973835947821d5444b40b04c65b3aee548b834e
guoliang-chen <Guoliang.Chen@netbrain.com>
2022-03-29 10:36:27 -0400

    Update - The latest import plugin

--- src/Feature_Data_Import - nb.py
+++ src/Feature_Data_Import - nb.py
@@ -1,8 +1,7 @@
 import glob
 import json
-# # from fileinput import filename
 import os
-import re
+import time
 import zipfile
 
 from netbrain.sysapi import (
@@ -13,15 +12,13 @@ from netbrain.sysapi import (
 )
 
 FDS_PATH = 'Feature_Design_Summary'
-NB_Install_Folder = r'C:\Program Files\NetBrain\Worker Server\Temp\UserData'
+NB_Install_Folder = r'C:\Program Files\NetBrain\Worker Server\UserData\Temp'
 
 DEBUG = True
-CONF_KEY = '_conf_'
-DEV_KEY = '_dev_'
-INTF_KEY = '_intf_'
+CONF_KEY = 'conf'
+DEV_KEY = 'dev'
+INTF_KEY = 'intf'
 INTF_TYPE = 'intfs'
-DEV_NAMEW_of_REGEX = re.compile(r'(.+?)_.*?_').search
-CMD_KEY_of_REGEX = re.compile(r'_(\S+ .*?)_').search
 
 
 def dpt(msg, level=pluginfw.INFO):
@@ -122,8 +119,9 @@ class ImportDeviceData:
         with open(r'%s' % conf_path) as f:
             config = f.read()
         if devicedata.ImportConfig(self._driver_id, config):
+            time.sleep(0.1)
             msg = 'Import configuration of %s successfully' % self._dev_name
-            dpt(msg, pluginfw.WARNING)
+            dpt(msg)
             return True
         else:
             msg = 'Failed to import configuration of %s' % self._dev_name
@@ -143,19 +141,33 @@ class ImportDeviceData:
             new_dev_obj = json.loads(f.read())
         dev_obj = datamodel.GetDeviceObject(self._dev_name)
         if not dev_obj:
-            msg = 'Failed to get device object of %s' % self._dev_name
-            dpt(msg, pluginfw.ERROR)
+            msg = 'No device %s was found, Preparing to create new device' % self._dev_name
+            dpt(msg, pluginfw.WARNING)
+            self.add_device_object(new_dev_obj)
             return False
         dev_id = dev_obj.get('_id')
         new_dev_obj['_id'] = dev_id
         if datamodel.SetDeviceObject(self._dev_name, new_dev_obj):
             msg = 'Update device properties of %s successfully' % self._dev_name
-            dpt(msg, pluginfw.WARNING)
+            dpt(msg)
             return True
         else:
             msg = 'Failed to update device properties of %s' % self._dev_name
             dpt(msg, pluginfw.ERROR)
+            msg = json.dumps(new_dev_obj, indent=2)
+            dpt(msg)
         return False
+
+    def add_device_object(self, device_object):
+        if datamodel.AddDeviceObject(device_object):
+            msg = 'Add device %s into database successfully' % self._dev_name
+            dpt(msg)
+            return True
+        else:
+            msg = 'Failed to add device %s into database' % self._dev_name
+            dpt(msg, pluginfw.ERROR)
+            msg = json.dumps(device_object, indent=2)
+            dpt(msg)
 
     def update_interfaces_object(self, interfaces_json_path):
         '''
@@ -164,29 +176,58 @@ class ImportDeviceData:
         @param interfaces_json_path(string): 'C:\\Program Files\\NetBrain\\Worker\\Temp\\UserData\\R1_intf_20220324.json'
         @return True or False
         '''
-        if not interfaces_json_path:
+        if not (interfaces_json_path and self._dev_name):
+            return False
+        dev_obj = datamodel.GetDeviceObject(self._dev_name)
+        if not dev_obj:
+            msg = 'Skip update interface, because lack of device %s' % self._dev_name
+            dpt(msg, pluginfw.ERROR)
             return False
         with open(r'%s' % interfaces_json_path) as f:
             new_intfs_obj = json.loads(f.read())
         for new_intf_obj in new_intfs_obj:
             intf_name = new_intf_obj.get('name')
-            old_intf_obj = datamodel.GetInterfaceObject(self._dev_name, intf_name, INTF_TYPE)
-            if not old_intf_obj:
-                old_intf_id = new_intf_obj.get('_id')
-            else:
-                old_intf_id = old_intf_obj.get('_id')
-            new_intf_obj['_id'] = old_intf_id
             if 'operateInfo' in new_intf_obj:
                 new_intf_obj.pop('operateInfo')
+            old_intf_obj = datamodel.GetInterfaceObject(self._dev_name, intf_name, INTF_TYPE)
+            if not old_intf_obj:
+                msg = 'No interface %s.%s was found, Preparing to create new interface' % (self._dev_name, intf_name)
+                dpt(msg, pluginfw.WARNING)
+                self.add_interface_object(new_intf_obj)
+                return False
+
+            new_intf_obj['_id'] = old_intf_obj.get('_id')
             if 'devId' in new_intf_obj:
                 new_intf_obj.pop('devId')
             if datamodel.SetInterfaceObject(self._dev_name, intf_name, INTF_TYPE, new_intf_obj):
                 msg = 'Update interface properties of %s.%s successfully' % (self._dev_name, intf_name)
-                dpt(msg, pluginfw.WARNING)
+                dpt(msg)
             else:
                 msg = 'Failed to update interface properties of %s.%s' % (self._dev_name, intf_name)
                 dpt(msg, pluginfw.ERROR)
+                msg = json.dumps(new_intf_obj, indent=2)
+                dpt(msg)
+
         return True
+
+    def add_interface_object(self, interface_object):
+        intf_name = interface_object.get('name')
+        if datamodel.AddPhantomInterface(self._dev_name, intf_name, INTF_TYPE, "L2_Topo_Type"):
+            msg = 'Add %s.%s into database successfully' % (self._dev_name, intf_name)
+            dpt(msg)
+            if datamodel.SetInterfaceObject(self._dev_name, intf_name, INTF_TYPE, interface_object):
+                msg = 'Update interface properties of %s.%s successfully' % (self._dev_name, intf_name)
+                dpt(msg)
+            else:
+                msg = 'Failed to update interface properties of %s.%s' % (self._dev_name, intf_name)
+                dpt(msg, pluginfw.ERROR)
+                msg = json.dumps(interface_object, indent=2)
+                dpt(msg)
+        else:
+            msg = 'Failed to add %s.%s into database' % (self._dev_name, intf_name)
+            dpt(msg, pluginfw.ERROR)
+            msg = json.dumps(interface_object, indent=2)
+            dpt(msg)
 
     def import_command(self, cli_command, command_txt_path):
         '''
@@ -206,7 +247,7 @@ class ImportDeviceData:
         protocoldata.original = cli_outputs
         if devicedata.ImportDeviceDataByCLI(protocoldata):
             msg = 'Import cli command <%s> of %s successfully' % (cli_command, self._dev_name)
-            dpt(msg, pluginfw.WARNING)
+            dpt(msg)
             return True
         else:
             msg = 'Failed to import cli command <%s> of %s' % (cli_command, self._dev_name)
@@ -218,10 +259,10 @@ def parse_input(input):
     input_info = json.loads(input)
     input_file_path = input_info.get('input_file_path', '')
     if not input_file_path:
-        print('Please put the input file in a specific path')
+        pluginfw.AddLog('Please put the input file in a specific path')
         return ''
     if not os.path.exists(input_file_path):
-        print('The input file could not be found at the specified path')
+        pluginfw.AddLog('The input file could not be found at the specified path')
         return ''
     unzip_folder = NB_Install_Folder + '\\' + FDS_PATH
     with zipfile.ZipFile(input_file_path, "r") as zipFile:
@@ -235,10 +276,30 @@ def init_devices_database(devices_js_list) -> dict:
     if not devices_js_list:
         return res
     for dev_json_path in devices_js_list:
-        dev_path = os.path.split(dev_json_path)[-1]
-        dev_name = dev_path[:dev_path.find(DEV_KEY)]
+        dev_name, datatype, timestamp = parse_device_info_by_filename(os.path.basename(dev_json_path))
         res.setdefault(dev_name, ImportDeviceData(dev_json_path))
     return res
+
+
+def translate_from_name(file_name):
+    translator = FileNameTranslate()
+    file_name = translator.translateToName(file_name)
+    return file_name
+
+
+def parse_device_info_by_filename(file_name):
+    file_name = os.path.splitext(file_name)[0]
+
+    # Parse index
+    _time_index = file_name.rfind('_')
+    _datatype_index = file_name[:_time_index].rfind('_')
+
+    # Find keywords base on index
+    deviceName = file_name[:_datatype_index]
+    dataType = file_name[_datatype_index + 1:_time_index]
+    timestamp = file_name[_time_index + 1::]
+
+    return (deviceName, dataType, timestamp)
 
 
 def run(input):
@@ -247,9 +308,10 @@ def run(input):
         return False
 
     data_folder = unzip_folder + '\\DeviceData'
+    pluginfw.AddLog(data_folder)
 
     for folderName, subfolders, filenames in os.walk(data_folder):
-        init_devices = init_devices_database(glob.glob(r'%s\*_dev_*.json' % folderName))
+        init_devices = init_devices_database(glob.glob(r'%s\*_%s_????????.json' % (folderName, DEV_KEY)))
         if not init_devices:
             msg = 'No device json was found in the %s' % folderName
             dpt(msg, pluginfw.ERROR)
@@ -258,21 +320,19 @@ def run(input):
         filenames = sorted(filenames)
         for file_name in filenames:
             file_full_path = os.path.join(folderName, file_name)
-            dev_name_search = DEV_NAMEW_of_REGEX(file_name)
-            dev_name = dev_name_search.group(1) if dev_name_search else ''
+            dev_name, datatype, timestamp = parse_device_info_by_filename(file_name)
             if dev_name not in init_devices:
                 msg = 'No device json of %s was found' % dev_name
                 dpt(msg, pluginfw.ERROR)
                 continue
             device_data = init_devices.get(dev_name)
-            if file_name.rfind(CONF_KEY) != -1:
+            if datatype == CONF_KEY:
                 device_data.import_config_object(file_full_path)
-            elif file_name.rfind(DEV_KEY) != -1:
+            elif datatype == DEV_KEY:
                 device_data.update_device_object(file_full_path)
-            elif file_name.rfind(INTF_KEY) != -1:
+            elif datatype == INTF_KEY:
                 device_data.update_interfaces_object(file_full_path)
-            elif CMD_KEY_of_REGEX(file_name):
-                cli_cmd = CMD_KEY_of_REGEX(file_name).group(1)
-                device_data.import_command(cli_cmd, file_full_path)
+            elif len(datatype.split()) > 1:
+                device_data.import_command(datatype, file_full_path)
 
     return True
